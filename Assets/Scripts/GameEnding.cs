5937e3c8075e0e90b04e74c6ff5a0e73dcfbbbdc
bmucf <br407069@ucf.edu>
2024-11-20 10:02:23 -0500

    Added a way for the game to end.

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPLayerAtExit;
+    float m_Timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player) 
+        {
+            m_IsPLayerAtExit = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPLayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
